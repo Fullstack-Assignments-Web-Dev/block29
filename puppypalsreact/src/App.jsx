dcaa7b08af3d6539885985e72d496c0c84a589c1
rdelaro <rachealdelarosa@yahoo.com>
2023-08-03 03:58:03 -0700

    first commit

---
+++ puppypalsreact/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bones'>
+                
+          <img src="./src/assets/bones2blue2.png" className="logo react" alt="Bones logo" />
+      
+      </div>
+      
+    </>
+  )
+}
+
+export default App
